6896b30b45a283a1459fbca89827eb800a4ff552
Ha Vy Le <havy.le@tum.de>
2025-06-19 14:43:14 +0200

    GET microservice on port 3031

--- books-get/main.go
+++ books-get/main.go
@@ -1,6 +1,20 @@
 package main
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"slices"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 // Defines a "model" that we can use to communicate with the
 // frontend or the database
@@ -13,4 +27,101 @@ type BookStore struct {
 	BookEdition string
 	BookPages   string
 	BookYear    string
+}
+
+// Here we make sure the connection to the database is correct and initial
+// configurations exists. Otherwise, we create the proper database and collection
+// we will store the data.
+// To ensure correct management of the collection, we create a return a
+// reference to the collection to always be used. Make sure if you create other
+// files, that you pass the proper value to ensure communication with the
+// database
+func prepareDatabase(client *mongo.Client, dbName string, collecName string) (*mongo.Collection, error) {
+	db := client.Database(dbName)
+
+	names, err := db.ListCollectionNames(context.TODO(), bson.D{{}})
+	if err != nil {
+		return nil, err
+	}
+	if !slices.Contains(names, collecName) {
+		cmd := bson.D{{Key: "create", Value: collecName}}
+		var result bson.M
+		if err = db.RunCommand(context.TODO(), cmd).Decode(&result); err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+	}
+
+	coll := db.Collection(collecName)
+	return coll, nil
+}
+
+// Generic method to perform "SELECT * FROM BOOKS" (if this was SQL, which
+// it is not :D ), and then we convert it into an array of map. In Golang, you
+// define a map by writing map[<key type>]<value type>{<key>:<value>}.
+// interface{} is a special type in Golang, basically a wildcard...
+func findAllBooks(coll *mongo.Collection) []map[string]interface{} {
+	cursor, err := coll.Find(context.TODO(), bson.D{{}})
+	var results []BookStore
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+
+	var ret []map[string]interface{}
+	for _, res := range results {
+		ret = append(ret, map[string]interface{}{
+			"ID":          res.MongoID.Hex(),
+			"BookName":    res.BookName,
+			"BookAuthor":  res.BookAuthor,
+			"BookEdition": res.BookEdition,
+			"BookPages":   res.BookPages,
+		})
+	}
+
+	return ret
+}
+
+func main() {
+	// Connect to the database. Such defer keywords are used once the local
+	// context returns; for this case, the local context is the main function
+	// By user defer function, we make sure we don't leave connections
+	// dangling despite the program crashing. Isn't this nice? :D
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// TODO: make sure to pass the proper username, password, and port
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	// This is another way to specify the call of a function. You can define inline
+	// functions (or anonymous functions, similar to the behavior in Python)
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// You can use such name for the database and collection, or come up with
+	// one by yourself!
+	coll, err := prepareDatabase(client, "exercise-1", "information")
+
+	// Here we prepare the server
+	e := echo.New()
+
+	// Log the requests. Please have a look at echo's documentation on more
+	// middleware
+	e.Use(middleware.Logger())
+
+	e.GET("/api/books", func(c echo.Context) error {
+		books := findAllBooks(coll)
+		return c.JSON(http.StatusOK, books)
+	})
+
+	// We start the server and bind it to port 3030. For future references, this
+	// is the application's port and not the external one. For this first exercise,
+	// they could be the same if you use a Cloud Provider. If you use ngrok or similar,
+	// they might differ.
+	// In the submission website for this exercise, you will have to provide the internet-reachable
+	// endpoint: http://<host>:<external-port>
+	fmt.Println("Books GET service starting on port 3030")
+	e.Logger.Fatal(e.Start(":3031"))
 }
